02eb6c2e1fa751324729966469c5af0d52a3055e
PettyDriedFish <131784162+PettyDriedFish@users.noreply.github.com>
2023-05-28 23:37:11 +0100

    704 finished

---
+++ 数组/704/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int result = Solution.search(new int[]{-1, 0, 3, 5, 9, 12}, 9);
+        System.out.println(result);
+    }
+}
---
+++ 数组/704/src/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int search(int[] nums, int target) {
+        //如果目标不在nums区域内，返回-1
+        if(target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+}
