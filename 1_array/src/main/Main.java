2853f74b444b78b38ace93fbafb4e4bd665438bc
PettyDriedFish <131784162+PettyDriedFish@users.noreply.github.com>
2023-05-29 16:16:46 +0100

    27 remove element completed

--- 1_array/src/main/Main.java
+++ 1_array/src/main/Main.java
@@ -3,7 +3,11 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        int result = Solution.search(new int[]{-1, 0, 3, 5, 9, 12}, 9);
-        System.out.println(result);
+        // 704
+//        int result = Solution.search(new int[]{-1, 0, 3, 5, 9, 12}, 9);
+//        System.out.println(result);
+
+        // 27
+        System.out.println(Solution.removeElement(new int[]{ 0, 1, 2, 2, 3, 0, 4, 2}, 2));
     }
 }
--- 1_array/src/main/Solution.java
+++ 1_array/src/main/Solution.java
@@ -1,8 +1,9 @@
 package main;
 
 class Solution {
+    // 704 二分查找
     public static int search(int[] nums, int target) {
-        //如果目标不在nums区域内，返回-1
+        // 如果目标不在nums区域内，返回-1
         if(target < nums[0] || target > nums[nums.length - 1]) {
             return -1;
         }
@@ -18,5 +19,33 @@ class Solution {
                 left = mid + 1;
         }
         return -1;
+    }
+
+    // 27 移除元素
+    public static int removeElement(int[] nums, int val) {
+        // 暴力解法：两层for循环，一个for循环遍历数组元素 ，第二个for循环更新数组
+//        int numsLength = nums.length;
+//        for(int i = 0; i < numsLength; i++) {
+//            if(nums[i] == val) {
+//                for(int j = i + 1; j < numsLength; j++) {
+//                    nums[j - 1] = nums[j];
+//                }
+//                i = i - 1;
+//                numsLength = numsLength - 1;
+//            }
+//        }
+//        return numsLength;
+
+        /* 快慢指针法：慢指针相当于新的数组下标，当一个与val不一样的数字出现，加一
+         * 快指针为旧的数组下标，每次判断使用
+         */
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if(nums[fastIndex] != val) {
+                nums[slowIndex] = nums[fastIndex];
+                slowIndex++;
+            }
+        }
+        return slowIndex;
     }
 }
