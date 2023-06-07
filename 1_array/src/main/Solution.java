d2e727dee4e5cf9b0190f06ee46392e3efc1e415
PettyDriedFish <131784162+PettyDriedFish@users.noreply.github.com>
2023-06-07 11:54:05 +0100

    209 completed

--- 1_array/src/main/Solution.java
+++ 1_array/src/main/Solution.java
@@ -85,4 +85,22 @@ class Solution {
         }
         return newNums;
     }
+
+    // 209 长度最小的子数组
+    public int minSubArrayLen(int target, int[] nums) {
+        //滑动窗口，最外层指针是指的尾巴嗷
+        int left = 0;
+        int sum = 0;
+        int result = Integer.MAX_VALUE;
+        for(int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while(sum >= target) {
+                result = Math.min(result, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        result = (result == Integer.MAX_VALUE) ? 0 : result;
+        return result;
+    }
 }
